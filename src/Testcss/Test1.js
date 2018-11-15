5a9a1f098932cb3a42d1562152a76f708f1fb63c
Tarsem Jangra <tarsemk.jangra@gmail.com>
2018-11-15 16:54:21 +0530

    this is modified file

--- src/Testcss/Test1.js
+++ src/Testcss/Test1.js
@@ -10,7 +10,36 @@ const Test1 = () =>{
 			<p className="p2">This paragraph is use to pdding</p> 
 
 			<div className="di">
-			<h1> This block element has height 200 px and widht is 100%</h1> 
+				<h1> This block element  has1 height 200 px and widht is 100%</h1>
+			</div> 
+			<div className="outline">
+				<p>This is use for otline </p>
+			</div>	
+			<div className="Justfy">
+				In my younger and more vulnerable years my father gave me some advice that
+				 I've been turning over in my mind ever since. 'Whenever you feel like criticizing
+				  anyone,' he told me, 'just remember that all the people in this world haven't had 
+				  the advantages that you've had.'
+			</div>
+			<div className="Shadow">
+				<h1>This is Shadow of the Text</h1>
+			</div>
+			<div className="fontSize">
+				<h1>This is Times New Roman serif</h1>
+				<h2 id="tr"> This is a paragraph, shown in the Arial font</h2>
+			</div>
+
+			<div className="icons">
+				<a href="#" className="fa fa-facebook"></a>
+				<a href="#" className="fa fa-twitter"></a>
+				<a href="#" className="fa fa-google"></a>
+				<a href="#" className="fa fa-linkedin"></a>
+				<a href="#" className="fa fa-youtube"></a>
+				<a href="#" className="fa fa-instagram"></a>
+				<a href="#" className="fa fa-pinterest"></a>
+				<a href="#" className="fa fa-snapchat-ghost"></a>
+				<a href="#" className="fa fa-skype"></a>
+				<a href="#" className="fa fa-android"></a>
 			</div>
 		</div>
 	);
